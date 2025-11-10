60cc9772d32f8311b560b875942ab1614199f822
Muhirwa <muhirwa232@gmail.com>
2025-11-10 22:22:40 +0200

    Refactor data preparation and encoding logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,16 +61,21 @@ with st.expander('Input features'):
 # Encode X
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-df_penguins
 
-# X = df_penguins[1:]
-# input_row = df_penguins[:1]
+X = df_penguins[1:]
+input_row = df_penguins[:1]
 
-# # Encode y
-# target_mapper = {'Adelie': 0,
-#                  'Chinstrap': 1,
-#                  'Gentoo': 2}
-# def target_encode(val):
-#   return target_mapper[val]
+# Encode y
+target_mapper = {'Adelie': 0,
+                 'Chinstrap': 1,
+                 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
 
-# y = y_raw.apply(target_encode)
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (input penguin)**')
+  input_row
+  st.write('**Encoded y**')
+  y
