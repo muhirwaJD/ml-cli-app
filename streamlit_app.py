7b2b09a5725fee78a771bdd6b49df5543bf907c6
Muhirwa <muhirwa232@gmail.com>
2025-11-10 22:19:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,7 @@ with st.expander('Input features'):
 # Encode X
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+df_penguins
 
 # X = df_penguins[1:]
 # input_row = df_penguins[:1]
