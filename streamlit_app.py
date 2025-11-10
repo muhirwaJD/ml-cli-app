24e0a2e571ed296b5266cdc0d5072267eb21dc92
Muhirwa <muhirwa232@gmail.com>
2025-11-10 21:44:49 +0200

    Add data frame loading from GitHub repository

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.title('Muhirwa')
 
 st.info('This is my first ML app')
+
+# Data frame from a github repo
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
